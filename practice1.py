7b5bdb1625bdfa4471a75de9e5b310913c98f9bf
muhdahmaddahlan <mudmud.muhd@gmail.com>
2018-12-24 14:23:14 +0800

    legends titles labels

--- practice1.py
+++ practice1.py
@@ -1,5 +1,18 @@
 import matplotlib.pyplot as plt
 
-plt.plot([1,2,3],[5,7,4])
+x = [1,2,3]
+y= [5,4,7]
 
+x2 = [1,2,3]
+y2 = [10,11,12]
+
+plt.plot(x,y,label = "First")
+plt.plot(x2,y2,label = "Second")
+
+
+plt.xlabel('x axis')
+plt.ylabel('y axis')
+
+plt.title('cool graph')
+plt.legend()
 plt.show()
