2f5149cc63a8f3119745c40190206960b5f08054
muhdahmaddahlan <mudmud.muhd@gmail.com>
2018-12-24 17:26:23 +0800

    stack plot

---
+++ practice4.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+days = [1,2,3,4,5]
+
+sleeping = [7,8,7,10,8]
+eating = [2,3,4,3,2]
+working = [7,8,7,8,8]
+playing = [3,4,5,2,6]
+
+
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('cool graph')
+plt.legend()
+plt.show()
