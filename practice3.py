cbfecb5eb6051b017a0dad6557c26f042164421b
muhdahmaddahlan <mudmud.muhd@gmail.com>
2018-12-24 14:49:59 +0800

    scatter plotz

--- practice3.py
+++ practice3.py
@@ -1,12 +1,14 @@
 import matplotlib.pyplot as plt
 
+x = [1,2,3,4,5,6,7,8]
+y = [2,3,4,5,6,7,8,9]
 
-
+plt.scatter(x,y,label ='skitscat', color= 'k',marker= "X", s =  100)
 
 
 plt.xlabel('x')
 plt.ylabel('y')
 
-plt.title('cool graph')
+plt.title('cool graph\nCheck it out sonny')
 plt.legend()
 plt.show()
