65112a07128f564329e4140652bed6141f8da397
muhdahmaddahlan <mudmud.muhd@gmail.com>
2018-12-24 17:31:17 +0800

    stackplot

--- practice4.py
+++ practice4.py
@@ -7,7 +7,14 @@ eating = [2,3,4,3,2]
 working = [7,8,7,8,8]
 playing = [3,4,5,2,6]
 
+#fake labels as stack plot does not allow labels :(
+plt.plot([],[], color = 'm', label = 'Sleeping', linewidth = 10)
+plt.plot([],[], color = 'c', label = 'Eating', linewidth = 10)
+plt.plot([],[], color = 'r', label = 'Working', linewidth = 10)
+plt.plot([],[], color = 'b', label = 'Playing', linewidth = 10)
 
+
+plt.stackplot(days,sleeping, eating , working, playing, colors= ['m','c','r','b'])
 
 plt.xlabel('x')
 plt.ylabel('y')
