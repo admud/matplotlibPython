6eb0fa0abf1af7647b61a26fd165a9ae4d94eeac
muhdahmaddahlan <mudmud.muhd@gmail.com>
2018-12-28 11:47:01 +0800

    gettinug data from the internet

--- practice7.py
+++ practice7.py
@@ -12,6 +12,17 @@ def graph_data(stock):
   stock_data = []
   split_source = source_code.split('\n')
 
+  for line in split_source:
+    split_line = line.split('')
+    if len(split_line) == 6:
+      if 'values' not in line:
+        stock_data.append(line)
+
+  date, closep,highp, lowp,openp,volume = np.loadtxt(stock_data,
+                                                     delimiter = ',',
+                                                     unpack = True,
+                                                     converters = {0: bytespdate2num('%Y%m%d')})
+
   plt.xlabel('x')
   plt.ylabel('y')
   plt.title('cool graph')
