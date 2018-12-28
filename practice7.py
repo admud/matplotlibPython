1b1258759fffa8c5548080ec415f80a4d63277a3
muhdahmaddahlan <mudmud.muhd@gmail.com>
2018-12-28 12:43:35 +0800

    convertind data from internet

--- practice7.py
+++ practice7.py
@@ -1,21 +1,32 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import urllib
+import urllib2
+
 import matplotlib.dates as mdates
+
+def bytespdate2num(fmt,encoding = 'utf-8'):
+  strconverter = mdates.strpdate2num(fmt)
+  def bytesconverter(b):
+    s = b.encode(encoding)
+    return strconverter(s)
+
+  return bytesconverter
+
 
 def graph_data(stock):
 
   stock_price_url = 'https://pythonprogramming.net/yahoo_finance_replacement'
 
-  source_code = urllib.request.urlopen(stock_price_url).read().decode()
-
+  #source_code = urllib.request.urlopen(stock_price_url).read().decode()
+  source_code = urllib2.urlopen(stock_price_url).read().decode()
   stock_data = []
   split_source = source_code.split('\n')
 
   for line in split_source:
-    split_line = line.split('')
+    split_line = line.split(',')
     if len(split_line) == 6:
-      if 'values' not in line:
+      if 'values' not in line and 'labels' not in line:
         stock_data.append(line)
 
   date, closep,highp, lowp,openp,volume = np.loadtxt(stock_data,
@@ -23,8 +34,11 @@ def graph_data(stock):
                                                      unpack = True,
                                                      converters = {0: bytespdate2num('%Y%m%d')})
 
-  plt.xlabel('x')
-  plt.ylabel('y')
+
+  plt.plot_date(date, closep,'-',label = 'Price')
+
+  plt.xlabel('date')
+  plt.ylabel('price')
   plt.title('cool graph')
   plt.legend()
   plt.show()
